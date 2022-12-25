7ac320f947f5f7cab84491d0554fffcb0e8b5420
Artem Viatkin <newyorkbox47@gmail.com>
2022-12-25 14:14:05 +0200

    Добавьте файлы проекта.

---
+++ lesson 8/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//понятие структура 
+//подходит для моделирования в приложении мматематических и геометрических и др атомарных сушностей
+
+Console.WriteLine("******A First Look Structures***** \n");
+//Создать переменную типа Point 
+Point myPoint;
+myPoint.X = 349;
+myPoint.Y = 76;
+myPoint.Display();
+
+//Скорректировать значение Х и Y
+myPoint.Increment();
+myPoint.Display();
+Console.ReadLine();
+
+struct Point
+{
+    //поля структуры 
+    public int X;
+    public int Y;
+
+    //добавить 1 к позиции (X,Y)
+    public void Increment()
+    {
+        X++; Y++;
+    }
+
+    //выченсть 1 из позиции (X,Y)
+    public void Decrement()
+    {
+        X--; Y--;
+    }
+
+    //Отобразить текущую позицию
+    public void Display()
+    {
+        Console.WriteLine("X={0}, Y={1}", X, Y);
+    }
+}
